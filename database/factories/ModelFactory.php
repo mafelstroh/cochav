e456d3ae4d3fc13debf37a6f64b30b180bb220f5
vagrant <vagrant@homestead>
2016-04-15 21:05:18 +0000

    Added Product seeder with a Factory definition, using Faker::class

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+// Model Factory for Products
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'product_name' 		  => $faker->name,
+        'product_description' => $faker->paragraph($nbSentences = 10),
+        'product_price'       => $faker->numberBetween(10, 20),
+        'product_quantity'    => $faker->numberBetween(10, 20),
+        'product_isactive'    => $faker->boolean
+
+    ];
+});
